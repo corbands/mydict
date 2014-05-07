9757d291f1150ab7ba8b07ea649c5575445b4ead
corbands <corbands@gmail.com>
2014-05-07 15:39:30 +0400

    +pycharm, +navbar, +signin error

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,12 @@ from users import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.auth, name='auth'),
-	url(r'auth/', views.auth, name='auth'),
-	url(r'home/', views.home, name='home'),
+	url(r'auth/$', views.auth, name='auth'),
+	url(r'home/$', views.home, name='home'),
 	url(r'register/$', views.register, name='register'),
 
-	url(r'register_check/', views.register_check, name='register_check'),
-	url(r'signout/', views.signout, name='signout'),
+    url(r'about/$', views.about, name='about'),
+
+	url(r'register_check/$', views.register_check, name='register_check'),
+	url(r'signout/$', views.signout, name='signout'),
 	)
--- users/views.py
+++ users/views.py
@@ -10,62 +10,76 @@ from django.contrib.auth.models import User
 from words.models import Word
 
 def auth(request):
-	return render(request, 'users/auth.html')
+    return render(request, 'users/auth.html')
 
 def home(request):
-	if not request.user.is_authenticated():
-		if not request.POST:
-			return HttpResponseRedirect(reverse('users:auth'))
-		
-		u = request.POST['username']
-		p = request.POST['password']
-		# try:
-			# p = request.POST['password1']
-		# except:
-			# p = request.POST['password']
+    error_msg = ''
+    msg_type = ''
 
-		user = authenticate(username=u, password=p)
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				error_msg = "User is valid, active and authenticated"
-			else:
-				error_msg = "The password is valid, but the account has been disabled!"
-		else:
-			error_msg = "The username and password were incorrect."
-	else:
-		user = request.user
-		error_msg = ''
+    if not request.user.is_authenticated():
+        if not request.POST:
+            return HttpResponseRedirect(reverse('users:auth'))
 
-		if request.method == 'POST':
-			if request.POST.__contains__('english') and request.POST.__contains__('russian'):
-				en = request.POST['english']
-				ru = request.POST['russian']
-				w = Word(english=en, russian=ru, pub_date=timezone.now(), user=request.user)
-				w.save()
+        u = request.POST['username']
+        p = request.POST['password']
+        if u == "" or p == "":
+            context = {'auth_failed':"login or password are empty"}
+            return render(request, 'users/auth.html', context)
 
-	word_list = Word.objects.filter(user=user)
-	return render(request, 'users/home.html', {'user':user, 'error_msg':error_msg, 'word_list':word_list})
+        # try:
+            # p = request.POST['password1']
+        # except:
+            # p = request.POST['password']
+
+        user = authenticate(username=u, password=p)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                error_msg = "User is valid, active and authenticated"
+            else:
+                error_msg = "The password is valid, but the account has been disabled!"
+                msg_type = "warning"
+        else:
+            context = {'auth_failed':"login or password are incorrect!"}
+            return render(request, 'users/auth.html', context)
+    else:
+        user = request.user
+
+        if request.method == 'POST':
+            if request.POST.__contains__('english') and request.POST.__contains__('russian'):
+                en = request.POST['english']
+                ru = request.POST['russian']
+                w = Word(english=en, russian=ru, pub_date=timezone.now(), user=request.user)
+                w.save()
+
+    word_list = Word.objects.filter(user=user)
+
+    context = {'user':user, 'error_msg':error_msg, 'msg_type':msg_type, 'word_list':word_list}
+    return render(request, 'users/home.html', context)
 
 def signout(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('users:auth'))
+    logout(request)
+    return HttpResponseRedirect(reverse('users:auth'))
 
 def register(request):	
-	return render(request, 'users/register.html')
+    return render(request, 'users/register.html')
 
 def register_check(request):
-	name = request.POST['username']
-	p = request.POST['password1']
-	p2 = request.POST['password2']
-	# todo check p equal p2
+    name = request.POST['username']
+    p = request.POST['password1']
+    p2 = request.POST['password2']
+    # todo check p equal p2
 
-	user = User.objects.create_user(name, '', p)
-	user.save()
+    user = User.objects.create_user(name, '', p)
+    user.save()
 
-	user_auth = authenticate(username=name, password=p)
-	if user_auth is not None:
-		if user_auth.is_active:
-			login(request, user_auth)
+    user_auth = authenticate(username=name, password=p)
+    if user_auth is not None:
+        if user_auth.is_active:
+            login(request, user_auth)
 
-	return HttpResponseRedirect(reverse('users:home'))
+    return HttpResponseRedirect(reverse('users:home'))
+
+
+def about(request):
+    return render(request, 'users/about.html')
