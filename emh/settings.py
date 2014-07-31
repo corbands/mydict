754638a47137dd09cf09d2d7e6e01fe5b9fa8416
corbands <corbands@gmail.com>
2014-07-31 17:12:33 -0400

    posgres user

--- emh/settings.py
+++ emh/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'emhdb',
-        'USER': 'corban',
+        'USER': 'corbanps',
         'PASSWORD': 'asd',
         'HOST': '127.0.0.1',
         'PORT': '5432',
